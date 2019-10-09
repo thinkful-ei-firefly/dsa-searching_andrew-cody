d5645d0a647ef9fcd7a8a74f4ee4c2a92c7959c1
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-09 11:55:44 -0500

    css added

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import './main.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {arrayInput: '[0, 1, 2, 3, 4, 5]', valueInput: '5'}
+    this.state = { arrayInput: '[0, 1, 2, 3, 4, 5]', valueInput: '5' }
     this.handleArrChange = this.handleArrChange.bind(this)
     this.handleValChange = this.handleValChange.bind(this)
   }
 
   handleArrChange(e) {
-    this.setState({arrayInput: e.target.value})
+    this.setState({ arrayInput: e.target.value })
   }
   handleValChange(e) {
-    this.setState({valueInput: e.target.value})
+    this.setState({ valueInput: e.target.value })
   }
 
   render() {
     const handleSubmit = (e) => {
       console.log('submit button pushed')
     }
-  
+
     const handleReset = (e) => {
       console.log('reset button pushed')
     }
@@ -28,11 +29,13 @@ class App extends React.Component {
       <div className="App">
         <form>
           <label htmlFor="arrayInput">Array to search in</label>
-          <input type="text" id="arrayInput" value="[0, 1, 2, 3, 4, 5]" onChange={(e) => {this.handleArrChange(e)}}></input>
+          <input type="text" id="arrayInput" value="[0, 1, 2, 3, 4, 5]" onChange={(e) => { this.handleArrChange(e) }}></input>
           <label htmlFor="valueInput">Value to search for</label>
-          <input type="text" id="valueInput" value="5" onChange={(e) => {this.handleValChange(e)}}></input>
-          <button type="button" onClick={(e) => {handleSubmit(e)}}>Submit</button>
-          <button type="button" onClick={(e) => {handleReset(e)}}>Reset</button>
+          <input type="text" id="valueInput" value="5" onChange={(e) => { this.handleValChange(e) }}></input>
+          <div className="buttonHolder">
+            <button type="button" onClick={(e) => { handleSubmit(e) }}>Submit</button>
+            <button type="button" onClick={(e) => { handleReset(e) }}>Reset</button>
+          </div>
         </form>
       </div>
     )
