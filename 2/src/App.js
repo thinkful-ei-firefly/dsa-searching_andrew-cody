611992e5e8779ef8ba6c103475b97ea9a75ffabe
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-09 11:06:55 -0500

    react work

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {arrayInput: '[0, 1, 2, 3, 4, 5]', valueInput: '5'}
+    this.handleArrChange = this.handleArrChange.bind(this)
+    this.handleValChange = this.handleValChange.bind(this)
+  }
+
+  handleArrChange(e) {
+    this.setState({arrayInput: e.target.value})
+  }
+  handleValChange(e) {
+    this.setState({valueInput: e.target.value})
+  }
+
+  render() {
+    const handleSubmit = (e) => {
+      console.log('submit button pushed')
+    }
+  
+    const handleReset = (e) => {
+      console.log('reset button pushed')
+    }
+
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="arrayInput">Array to search in</label>
+          <input type="text" id="arrayInput" value="[0, 1, 2, 3, 4, 5]" onChange={(e) => {this.handleArrChange(e)}}></input>
+          <label htmlFor="valueInput">Value to search for</label>
+          <input type="text" id="valueInput" value="5" onChange={(e) => {this.handleValChange(e)}}></input>
+          <button type="button" onClick={(e) => {handleSubmit(e)}}>Submit</button>
+          <button type="button" onClick={(e) => {handleReset(e)}}>Reset</button>
+        </form>
+      </div>
+    )
+  };
 }
 
 export default App;
