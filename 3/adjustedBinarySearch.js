ab163dba8111f44d9e9dc762290d0b262aeed140
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-09 15:06:22 -0500

    adjustedBinSearch work

--- 3/adjustedBinarySearch.js
+++ 3/adjustedBinarySearch.js
@@ -1,18 +1,9 @@
 //input same except array can have same values in a row
 //output should be object: {start: firstIndexTheValueMatches, end: lastIndexTheValueMatches}
 
-const bookList = [
-    { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
-    { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
-    { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
-    { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
-    { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
-    { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
-    { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
-    { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
-    { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane\'s Fighting Ships' },
-    { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
-  ]
+//input example 
+const arrayExample = [0, 1, 2, 5, 5, 5, 7, 9, 10, 10, 12, 15, 18, 18]
+const valueExample = 5
 
 const adjustedBinarySearch = (array, value, start, end) => {
     var start = start === undefined ? 0 : start;
@@ -29,17 +20,17 @@ const adjustedBinarySearch = (array, value, start, end) => {
 
     
     //console.log(start, end);
-    if (item.dewey == value) {
+    if (item == value) {
         const anchorIndex = index;
-        let start = anchorIndex
-        let end = anchorIndex
+        let s = 0
+        let e = anchorIndex
         //do some search to find start index
-        while(array[index - 1].dewey === item.dewey){
-            start -= index
+        while(array[index - 1] === item){
+            s -= index
         }
         //do some search to find end index
-        while(array[index + 1].dewey === item.dewey){
-            start += index
+        while(array[index + 1] === item){
+            e += index
         }
         return {start: startIndex, end: endIndex}
         // return index
